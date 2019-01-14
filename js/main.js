7ce4173b33e12de513bfc8ce7a96be026a5dea15
sebastian <srodriguez@spartaglobal.com>
2019-01-14 22:33:49 +0000

    completed task - added basic '+,-,*,/', bmi and tempereature conversion modes - used while loop - used control flow - used prompts - used functions

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@
 
 var calc = confirm("would you like to use The calculator?")
 
-while (calc == true){
+while (calc == true) {
 
     var useCalc = prompt("Do you need Calculator Mode, Conversion Mode or BMI Mode")
 
-    if (useCalc == "calculator"){
+    if (useCalc == "calculator") {
 
         function myFunction(x, y, operator) {
             var z;
@@ -27,7 +27,7 @@ while (calc == true){
             return z;
         }
         var x, y, operator;
-        x = parseFloat(prompt("Type a number"));     
+        x = parseFloat(prompt("Type a number"));
         operator = prompt("Choose an operator");
         y = parseFloat(prompt("Type a number"));
 
@@ -35,35 +35,35 @@ while (calc == true){
     }
 
     else if (useCalc == "conversion") {
-    
+
         function herFunction(a) {
             var z;
 
             // x = 1.8;
             // y = 32;
-            z = (a * 1.8 ) + 32;
+            z = (a * 1.8) + 32;
 
             return z;
         }
         var a;
         a = parseFloat(prompt("Type a number"));
-        
+
         alert(herFunction(a));
     }
 
     else if (useCalc == "BMI") {
-    
+
         function hisFunction(x, y) {
             var z;
 
-            z = x/(y^2)
+            z = x / (y ^ 2)
 
             return z;
         }
         var x, y;
-        x = parseFloat(prompt("Your Weight in Kilograms"));     
+        x = parseFloat(prompt("Your Weight in Kilograms"));
         y = parseFloat(prompt("Your Hight in Meters"));
-        
+
         alert(hisFunction(x, y));
     }
 }
